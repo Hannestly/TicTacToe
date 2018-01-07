907e26dd4b85f04e2dde6f33b4697190127cb253
Hyon Lee <dooley994@gmail.com>
2018-01-07 01:47:40 -0500

    added replay menu and draw option of the game

--- app/src/main/java/com/hyonlee/connectthree/MainActivity.java
+++ app/src/main/java/com/hyonlee/connectthree/MainActivity.java
@@ -2,8 +2,13 @@ package com.hyonlee.connectthree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     // each represent a spot on the board, 2 meaning unplayed box
     int[] gameState = {2,2,2,2,2,2,2,2,2};
     int[][] winningState = {{0,3,6},{1,4,7},{2,5,8},{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6}};
+    boolean gameIsActive = true;
 
     public void dropIn(View view){
 
@@ -24,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         int tappedCounter = (Integer.parseInt(box.getTag().toString()))-1;
         System.out.println(Integer.toString(tappedCounter));
-        if (gameState[tappedCounter] == 2) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
             gameState[tappedCounter] = turn;
             box.setTranslationY(-1000f);
 
@@ -41,19 +47,55 @@ public class MainActivity extends AppCompatActivity {
                 turn = 0;
             }
 
-        }else{
-            Toast.makeText(getApplicationContext(), "The spot is occupied, please select another tile", Toast.LENGTH_SHORT).show();
         }
 
         for(int[] winningPosition : winningState){
             if(gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                gameIsActive = false;
+                LinearLayout winLayout = (LinearLayout) findViewById(R.id.winLayout);
+                TextView winText = (TextView) findViewById(R.id.winText);
                 if(gameState[winningPosition[0]] == 0){
-                    Toast.makeText(getApplicationContext(), "Player Yellow has won!", Toast.LENGTH_SHORT).show();
+                    winText.setText("Yellow player has won!");
+
                 }else{
-                    Toast.makeText(getApplicationContext(), "Player Red has won!", Toast.LENGTH_SHORT).show();
+                    winText.setText("Red player has won!");
                 }
+
+                winLayout.setAlpha(1f);
             }
         }
+        boolean gameIsOver = true;
+        for ( int counterState: gameState) {
+
+            if (counterState == 2) {
+                gameIsOver = false;
+            }
+        }
+            if (gameIsOver){
+                gameIsActive = false;
+
+                LinearLayout winLayout = (LinearLayout) findViewById(R.id.winLayout);
+                TextView winText = (TextView) findViewById(R.id.winText);
+                winText.setText("The game is a draw");
+                winLayout.setAlpha(1f);
+            }
+
+    }
+
+    public void refresh(View view){
+        turn = 0;
+        for(int i = 0; i< gameState.length; i++){
+            gameState[i] = 2;
+        }
+        gameIsActive = true;
+        LinearLayout winLayout = (LinearLayout) findViewById(R.id.winLayout);
+        winLayout.setAlpha(0f);
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+        for(int i = 0; i<gridLayout.getChildCount(); i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
+
+
 
     }
     @Override
